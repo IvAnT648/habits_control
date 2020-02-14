c5dfb9e2056dedcd75b4176dc614f7a4406371a8
Иван <ant.ivan99@mail.ru>
2020-02-15 00:50:39 +0300

    Create form views changed; home page is set to default

--- resources/views/targets/new.blade.php
+++ resources/views/targets/new.blade.php
@@ -6,7 +6,7 @@
         <h1>Create new target</h1>
         <br><br>
     </div>
-    <div class="container col-md-6">
+    <div class="container-fluid col-5">
         <form class="form-horizontal" action="{{ route('targets.store') }}" method="post">
             @csrf
             @include('targets.partials.form')
--- resources/views/targets/partials/form.blade.php
+++ resources/views/targets/partials/form.blade.php
@@ -1,10 +1,33 @@
-<label>Target title</label>
-<input name="title" type="text" value="{{ $target->title ?? '' }} required">
-
-<label>Target description</label>
-<textarea name="description">{{ $target->description ?? '' }}</textarea>
-
-<label>What reasons?</label>
-<textarea name="reasons">{{ $target->reasons ?? '' }}</textarea>
-
-<button type="submit">Save</button>
+<div class="form-group row">
+    <label for="title" class="col-3 col-form-label-lg">Target title</label>
+    <div class="col-sm-9">
+        <input id="title" name="title" class="form-control" type="text" value="{{ $target->title ?? '' }}" required>
+        <small id="titleHelpBlock" class="form-text text-muted">
+            Specify your target's name.
+        </small>
+    </div>
+</div>
+<br>
+<div class="form-group row">
+    <label for="description" class="col-sm-3 col-form-label-lg">Target description</label>
+    <div class="col-sm-9">
+        <textarea id="description" name="description" class="form-control">{{ $target->description ?? '' }}</textarea>
+        <small id="descriptionHelpBlock" class="form-text text-muted">
+            Describe your target here.
+        </small>
+    </div>
+</div>
+<br>
+<div class="form-group row">
+    <label for="reasons" class="col-sm-3 col-form-label-lg">What reasons?</label>
+    <div class="col-sm-9">
+        <textarea id="reasons" name="reasons" class="form-control">{{ $target->reasons ?? '' }}</textarea>
+        <small id="titleHelpBlock" class="form-text text-muted">
+            Describe reasons why you stand the target.
+        </small>
+    </div>
+</div>
+<br>
+<div class="form-group">
+    <button type="submit" class="btn btn-primary btn-lg">Save</button>
+</div>
