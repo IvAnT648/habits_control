6baca11760e44efd6f4c60c779c3719ebf439023
Иван <ant.ivan99@mail.ru>
2020-02-19 01:02:28 +0300

    Layout: changed styles

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container col-lg-6">
     <h1>Activity's table page</h1>
+    @component('components.breadcrumb', ['items' => [['route' => 'home', 'text' => 'Main'], ['text' => 'Activity']]]) @endcomponent
+    <hr>
 </div>
 @endsection
--- resources/views/targets/index.blade.php
+++ resources/views/targets/index.blade.php
@@ -2,18 +2,16 @@
 
 @section('content')
     <div class="container col-lg-6">
-
         <h2>List of targets</h2>
         @component('components.breadcrumb', ['items' => [['route' => 'home', 'text' => 'Main'], ['text' => 'Target']]]) @endcomponent
-
         <hr>
-
         <a href="{{ route('targets.create') }}" class="btn btn-primary pull-right"><i class="fa fa-plus-square-o"></i>  Create a target</a>
-        <table class="table table-striped">
+        <br><br>
+        <table class="table table-light border">
             <thead>
             <th>Target title</th>
             <th>Description</th>
-            <th class="text-right">Action</th>
+            <th>Action</th>
             </thead>
             <tbody>
             @forelse ($targets as $target)
@@ -21,16 +19,15 @@
                     <td>{{ $target->title }}</td>
                     <td>{{ $target->description }}</td>
                     <td>
-                        <a href="{{ route('targets.edit', ['target' => $target->target_id]) }}"><i class="fa fa-pencil pull-right"></i></a>
+                        <a href="{{ route('targets.edit', ['target' => $target->target_id]) }}"><i class="fa fa-pencil fa-2x"></i></a>
                     </td>
                 </tr>
             @empty
                 <tr>
-                    <td colspan="3" class="text-center"><h2>Data not found</h2></td>
+                    <td colspan="3" class="text-center"><h5 style="font-style: italic">Data not found</h5></td>
                 </tr>
             @endforelse
             </tbody>
         </table>
     </div>
-
 @endsection
