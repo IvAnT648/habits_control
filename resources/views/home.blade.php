f4cdf6ff061ec32bf90de8bb1e055525b4bc86bd
Иван <ant.ivan99@mail.ru>
2020-02-19 18:12:37 +0300

    Home page corrected

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-sm-6">
             <a href="{{ url('activity') }}" class="list-group-item">
-                <h4 class="list-group-item-heading">Перейти к таблице активности</h4>
+                <h4 class="list-group-item-heading">Activity</h4>
             </a>
         </div>
         <div class="col-sm-6">
             <a href="{{ url('targets') }}" class="list-group-item">
-                <h4 class="list-group-item-heading">Перейти к редактированию целей</h4>
+                <h4 class="list-group-item-heading">Targets</h4>
             </a>
         </div>
     </div>
