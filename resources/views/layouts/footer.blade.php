490d8c30a282802d840787fac92fb89179c2700e
Ivan Antyukhin <ant.ivan99@mail.ru>
2020-06-16 18:45:26 +0300

    Footer text changed

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="page-footer font-small fixed-bottom bg-white shadow-sm">
     <div class="footer-copyright text-center py-3">
-        Habity © 2020
+        Habity © 2020 (training project)
     </div>
 </footer>
